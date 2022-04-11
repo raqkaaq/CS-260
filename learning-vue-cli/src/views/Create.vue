310f43553e9bdd30bfb021ff0f2af89e187839ce
raqkaaq <elijahcryan@gmail.com>
2022-04-11 14:30:03 -0600

    this

---
+++ learning-vue-cli/src/views/Create.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>Create a ticket</h1>
+        <form v-if="creating" @submit.prevent="addTicket">
+            <input v-model="name" placeholder="Name">
+            <p></p>
+            <textarea v-model="problem"></textarea>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+        <div v-else>
+            <p>Thanks for giving us another ticket, yay more work.</p>
+            <p><a @click="toggleForm" href="#">Submit another ticket</a></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'create-vue',
+    data() {
+        return{
+            creating: true,
+            name: '',
+            problem: '',
+        }
+    },
+    methods: {
+        toggleForm(){
+            this.creating = !this.creating;
+        },
+        addTicket(){
+            this.$root.$data.addTicket(this.name, this.problem);
+            this.name = '';
+            this.problem = '';
+            this.creating = false;
+        },
+    }
+}
+</script>
+<style scoped>
+input {
+    font-size: 1.2em;
+    height: 30px;
+    width: 200px;
+}
+textarea{
+    font-size: 1.6em;
+    width: 100%;
+    max-width: 500px;
+    height: 100px;
+}
+button{
+    margin-top: 20px;
+    font-size: 1.2em;
+}
+</style>
---
+++ learning-vue-cli/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Current Tickets</h1>
+    <div v-for="ticket in tickets" :key="ticket.id">
+      <hr>
+      <div class="ticket">
+        <p>{{ticket.problem}}</p>
+        <p> <i>-- {{ticket.name}}</i></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ticket-home',
+  data() {
+    return {}
+  },
+  computed: {
+    tickets() {
+      return this.$root.$data.getTickets();
+    }
+  }
+}
+</script>
